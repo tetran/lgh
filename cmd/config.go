4a581be59f09b6c6401ae00b03da2df3d40d4b9f
Koichi Kaneshige <coarse.ground@gmail.com>
2024-04-26 21:12:07 +0900

    Add config value

--- cmd/config.go
+++ cmd/config.go
@@ -9,6 +9,7 @@ import (
 	"path/filepath"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"github.com/tetran/lgh/internal/config"
 )
 
@@ -21,34 +22,24 @@ var configCmd = &cobra.Command{
 
 func init() {
 	configCmd.Flags().StringP("openai-api-key", "", "", "OpenAI API key")
-	configCmd.Flags().StringP("openai-model", "", "", "OpenAI model")
+	configCmd.Flags().StringP("claude-api-key", "", "", "Claude API key")
 	configCmd.Flags().StringP("lang", "", "", "Output language")
 }
 
 func configure(cmd *cobra.Command, args []string) {
-	key, err := cmd.Flags().GetString("openai-api-key")
+	okey, err := cmd.Flags().GetString("openai-api-key")
 	cobra.CheckErr(err)
 
-	model, err := cmd.Flags().GetString("openai-model")
+	ckey, err := cmd.Flags().GetString("claude-api-key")
 	cobra.CheckErr(err)
 
 	lng, err := cmd.Flags().GetString("lang")
 	cobra.CheckErr(err)
 
-	if key == "" && lng == "" && model == "" {
-		fmt.Println("Please enter the OpenAI API key and the output language.")
-		fmt.Print("OpenAI API key: ")
-		fmt.Scanln(&key)
-		fmt.Print("Please enter the OpenAI model (default: gpt-3.5-turbo): ")
-		fmt.Scanln(&model)
-		if model == "" {
-			model = "gpt-3.5-turbo"
-		}
-		fmt.Print("Output language (available: [en/ja], default: en): ")
-		fmt.Scanln(&lng)
-		if lng == "" {
-			lng = "en"
-		}
+	if okey == "" && ckey == "" && lng == "" {
+		okey = read("openai-api-key", "OpenAI API key: ", "Please enter the OpenAI API key and the output language.", "")
+		ckey = read("claude-api-key", "Claude API key: ", "Please enter the Claude API key.", "")
+		lng = read("lang", "Output language (available: [en/ja], default: en): ", "", "en")
 	}
 
 	file := filepath.Join(os.Getenv("HOME"), config.WorkDir, "config.yaml")
@@ -64,6 +55,23 @@ func configure(cmd *cobra.Command, args []string) {
 	cobra.CheckErr(err)
 	defer f.Close()
 
-	_, err = f.WriteString(fmt.Sprintf("openai-api-key: %s\nlang: %s\nopenai-model: %s\n", key, lng, model))
+	_, err = f.WriteString(fmt.Sprintf("openai-api-key: %s\nclaude-api-key: %s\nlang: %s\n", okey, ckey, lng))
 	cobra.CheckErr(err)
+}
+
+func read(name, label, desc, defaultV string) string {
+	if desc != "" {
+		fmt.Println(desc)
+	}
+	fmt.Print(label)
+
+	var val string
+	fmt.Scanln(&val)
+	if val == "" {
+		val = viper.GetString(name)
+		if val == "" && defaultV != "" {
+			val = defaultV
+		}
+	}
+	return val
 }
